d2c35306daee54c5f46b1b2d2e3b096e67d954f3
Banana Viking <bananavikingcoder@gmail.com>
2021-08-11 11:39:12 -0500

    Initial Commit

---
+++ Petstagram/PetstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetstagramApp.swift
+//  Petstagram
+//
+//  Created by J S on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct PetstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
