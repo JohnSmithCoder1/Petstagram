97c4cc9904b0b053982e9de0d7eaca2fa01d3ba7
John Smith <johnsmithcoder1@gmail.com>
2021-08-11 14:35:24 -0500

    Create JsonData

---
+++ PetstagramTests/JsonData.swift
@@ -0,0 +1,30 @@
+//
+//  JsonData.swift
+//  PetstagramTests
+//
+//  Created by J S on 8/11/21.
+//
+
+import Foundation
+
+enum JsonData {
+    static let goodFeed = """
+    [
+        {
+            "photoUrl": "/photos/image1.jpg",
+            "createdAt": "2020-04-01T12:00:00Z",
+            "caption": "Living her best life! #corgi #puppyStyle"
+        },
+        {
+            "photoUrl": "/photos/image2.jpg",
+            "createdAt": "2020-03-11T04:44:00Z",
+            "caption": "Bath time is best time!"
+        },
+        {
+            "photoUrl": "/photos/image3.jpg",
+            "createdAt": "2020-01-03T17:32:00Z",
+            "caption": "Not sure if alien or dog..."
+        }
+    ]
+    """
+}
