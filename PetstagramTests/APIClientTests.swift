716be9f48f50a43f6c91f325f84d4184dcab3d43
John Smith <johnsmithcoder1@gmail.com>
2021-08-11 16:37:13 -0500

    Add Swifter package and more tests to APIClientTests

--- PetstagramTests/APIClientTests.swift
+++ PetstagramTests/APIClientTests.swift
@@ -6,27 +6,67 @@
 //
 
 import XCTest
+import Swifter
+import Combine
+@testable import Petstagram
 
 class APIClientTests: XCTestCase {
-
+    var server = HttpServer()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        server.notFoundHandler = { [weak self] request in
+            let attachment = XCTAttachment(string: "\(request.method) on \(request.path) requested, but not found")
+            attachment.name = "Unhandled API path requested"
+            attachment.lifetime = .keepAlways
+            self?.add(attachment)
+            
+            return .notFound
+        }
+        
+        do {
+            try server.start()
+        } catch {
+            XCTFail("Could not start Swifter")
         }
     }
 
+    override func tearDownWithError() throws {
+        server.stop()
+    }
+
+    func testPublisherForRequest() {
+        server.GET["/api/v1/posts"] = { _ in HttpResponse.ok(.text(JsonData.goodFeed)) }
+        
+        let request = PostRequest()
+        let client = APIClient(environment: .local)
+        
+        var networkResult: Subscribers.Completion<Error>?
+        var fetchedPosts: [Post]?
+        let networkExpectation = expectation(description: "Network request")
+        
+        let cancellable = client.publisherForRequest(request)
+            .sink { result in
+                networkResult = result
+                XCTAssertTrue(Thread.current.isMainThread, "Network completion called on background thread")
+                networkExpectation.fulfill()
+            } receiveValue: { posts in
+                fetchedPosts = posts
+            }
+        
+        wait(for: [networkExpectation], timeout: 3)
+        cancellable.cancel()
+        
+        guard let result = networkResult else {
+            XCTFail("No result from network request")
+            
+            return
+        }
+        
+        switch result {
+        case .finished:
+            XCTAssertEqual(fetchedPosts?.count ?? 0, 3)
+        case .failure(let error):
+            XCTFail(error.localizedDescription)
+        }
+    }
 }
