a8c77ab5bce8f45ef5c9acbc9057d77683e8aa2e
John Smith <johnsmithcoder1@gmail.com>
2021-08-13 11:58:21 -0500

    Add image assets for tab view

--- Petstagram/UI/MainView.swift
+++ Petstagram/UI/MainView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import Combine
 
-struct ContentView: View {
+struct MainView: View {
     @State var showingLogin = true
     
     let signInPublisher = NotificationCenter.default
@@ -48,6 +48,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
