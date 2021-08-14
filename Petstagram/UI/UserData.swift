6f58949247a6d2ec0ac06d3809d88c5f0f4088b7
John Smith <johnsmithcoder1@gmail.com>
2021-08-14 12:15:45 -0500

    Create UserData.swift

---
+++ Petstagram/UI/UserData.swift
@@ -0,0 +1,13 @@
+//
+//  UserData.swift
+//  Petstagram
+//
+//  Created by J S on 8/14/21.
+//
+
+import Foundation
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var selectedTab = 0
+}
