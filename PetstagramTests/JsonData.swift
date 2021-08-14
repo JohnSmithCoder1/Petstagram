a5bca9f16fd5f7f1ee2432a5b094163037146b0b
John Smith <johnsmithcoder1@gmail.com>
2021-08-14 10:20:33 -0500

    Add placeholder posts to feed preview

--- PetstagramTests/JsonData.swift
+++ PetstagramTests/JsonData.swift
@@ -11,17 +11,17 @@ enum JsonData {
     static let goodFeed = """
     [
         {
-            "photoUrl": "/photos/image1.jpg",
+            "id": "43C5322C-ACB0-4A10-90A5-69BE4C769ED1",
             "createdAt": "2020-04-01T12:00:00Z",
             "caption": "Living her best life! #corgi #puppyStyle"
         },
         {
-            "photoUrl": "/photos/image2.jpg",
+            "id": "81B88642-59B3-4242-BE25-4B61F57E3965",
             "createdAt": "2020-03-11T04:44:00Z",
             "caption": "Bath time is best time!"
         },
         {
-            "photoUrl": "/photos/image3.jpg",
+            "id": "3452745A-CDFC-41B9-9604-40EA61CF8A06",
             "createdAt": "2020-01-03T17:32:00Z",
             "caption": "Not sure if alien or dog..."
         }
