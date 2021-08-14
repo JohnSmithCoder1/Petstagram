98f44831e9d24236e256d3feeb220d1d5766d376
John Smith <johnsmithcoder1@gmail.com>
2021-08-14 13:05:29 -0500

    Create CreatePostView.swift

---
+++ Petstagram/UI/Post/CreatePostView.swift
@@ -0,0 +1,20 @@
+//
+//  CreatePostView.swift
+//  Petstagram
+//
+//  Created by J S on 8/14/21.
+//
+
+import SwiftUI
+
+struct CreatePostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreatePostView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreatePostView()
+    }
+}
