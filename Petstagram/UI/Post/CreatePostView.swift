9ebb8c8cf0332fe4eb95676055caedfc5a781d0d
John Smith <johnsmithcoder1@gmail.com>
2021-08-14 13:09:53 -0500

    Add NavigationView to CreatePostView

--- Petstagram/UI/Post/CreatePostView.swift
+++ Petstagram/UI/Post/CreatePostView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct CreatePostView: View {
+    @State var postImage = UIImage()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            TakePhotoView(onPhotoCapture: { image in
+                postImage = image
+            })
+        }
     }
 }
 
