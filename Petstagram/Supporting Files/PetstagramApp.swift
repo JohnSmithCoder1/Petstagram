258a0ac5b916181445ebfabd9c9fe176e3a3efb8
John Smith <johnsmithcoder1@gmail.com>
2021-08-15 13:37:10 -0500

    Add server logic to PetstagramApp.swift

--- Petstagram/Supporting Files/PetstagramApp.swift
+++ Petstagram/Supporting Files/PetstagramApp.swift
@@ -6,9 +6,26 @@
 //
 
 import SwiftUI
+import Swifter
 
 @main
 struct PetstagramApp: App {
+    // TODO: temp server code until API is ready
+    // TODO: remove JsonData from target when this is removed
+    let server: HTTPServer = {
+        let server = HttpServer()
+        
+        // Use a different port so unit tests will still work
+        try? server.start(8081)
+        try? server.start(8081)
+        server.GET["/api/v1/posts"] = { _ in HttpResponse.ok(.text(JsonData.goodFeed)) }
+        server.POST["/api/v1/user"] = { _ in HttpResponse.ok(.text(JsonData.goodSignUp)) }
+        server.POST["/api/v1/posts"] = { _ in HttpResponse.ok(.text(JsonData.postUploaded)) }
+        server.POST["/api/v1/image"] = { _ in HttpResponse.ok(.text("")) }
+        
+        return server
+    }()
+    
     var body: some Scene {
         WindowGroup {
             MainView()
