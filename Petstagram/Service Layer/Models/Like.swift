5c991ed3ec44b5feaa2d3da793c1748e756c9d7f
John Smith <johnsmithcoder1@gmail.com>
2021-08-21 09:17:55 -0500

    Create Like.swift

---
+++ Petstagram/Service Layer/Models/Like.swift
@@ -0,0 +1,22 @@
+//
+//  Like.swift
+//  Petstagram
+//
+//  Created by J S on 8/21/21.
+//
+
+import Foundation
+
+struct Like: Codable {
+    var id: UUID?
+    var postId: UUID
+    var createdByUser: String
+    var createdAt: Date
+    
+    init(id: UUID? = nil, postId: UUID, createdBy: String = currentUser?.id ?? "", createdAt: Date = Date()) {
+        self.id = id
+        self.postId = postId
+        self.createdByUser = createdBy
+        self.createdAt = createdAt
+    }
+}
