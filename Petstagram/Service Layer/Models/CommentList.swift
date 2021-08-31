0ff904b51de0e3b88805e26bae86d09b248d7209
John Smith <johnsmithcoder1@gmail.com>
2021-08-31 10:20:49 -0500

    Create CommentList.swift

---
+++ Petstagram/Service Layer/Models/CommentList.swift
@@ -0,0 +1,37 @@
+//
+//  CommentList.swift
+//  Petstagram
+//
+//  Created by J S on 8/24/21.
+//
+
+import Foundation
+import Combine
+
+class CommentList: ObservableObject {
+    @Published var comments: [Comment] = []
+    @Published var loadError: Error?
+    let postId: UUID
+    private var subscriptions: Set<AnyCancellable> = []
+    
+    init(forPostId postId: UUID) {
+        self.postId = postId
+        loadComments()
+    }
+    
+    func loadComments() {
+        let client = APIClient()
+        let request = GetCommentsForPostRequest(postId: postId)
+        client.publisherForRequest(request)
+            .sink(receiveCompletion: { result in
+                if case .failure(let error) = result {
+                    self.loadError = error
+                } else {
+                    self.loadError = nil
+                }
+            }, receiveValue: { newComments in
+                self.comments = newComments
+            })
+            .store(in: &subscriptions)
+    }
+}
